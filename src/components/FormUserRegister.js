4059e20847be6827d1d41c2e08ad11986ed48624
nhht77 <35263094+nhht77@users.noreply.github.com>
2018-11-29 11:20:34 +0200

    validation password

--- src/components/FormUserRegister.js
+++ src/components/FormUserRegister.js
@@ -9,6 +9,7 @@ import IconButton from '@material-ui/core/IconButton';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import InputAdornment from '@material-ui/core/InputAdornment';
+import FormHelperText from '@material-ui/core/FormHelperText';
 
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
@@ -17,16 +18,25 @@ import VisibilityOff from '@material-ui/icons/VisibilityOff';
 export class FormUserDetails extends Component {
     state = {
         showPassword: false,
+        isMatch: true,
+        password:''
       };  
     
   continue = e => {
     e.preventDefault();
+    if(this.state.password !== this.props.values.password){
+      console.log('Password does not match!')
+    }
     this.props.nextStep();
   };
 
   handleClickShowPassword = () => {
     this.setState(state => ({ showPassword: !state.showPassword }));
   };
+
+  handlePaswordChange = e => {
+    this.setState({password: e.target.values})
+  }
 
   render() {
     const { values, handleChange} = this.props;
@@ -60,6 +70,19 @@ export class FormUserDetails extends Component {
               </InputAdornment>
             }
           />
+          {this.state.isMatch ? null : <FormHelperText id="component-error-text">Password is not Match</FormHelperText> }
+          </FormControl>
+          <br />
+          <FormControl style={styles.Input}>
+          <InputLabel htmlFor="adornment-password">Confirm Your Password</InputLabel>
+          <Input
+            id="adornment-password"
+            type='password'
+            
+            defaultValue={this.state.password}
+            onChange={handleChange(this.state.password)}
+          />
+          {this.state.isMatch ? null : <FormHelperText id="component-error-text">Password is not Match</FormHelperText> }
           </FormControl>
           <br />
           <RaisedButton
@@ -77,6 +100,10 @@ export class FormUserDetails extends Component {
 const styles = {
   button: {
     margin: 15
+  },
+  Input:{
+    width: 256,
+    margin:15
   }
 };
 
